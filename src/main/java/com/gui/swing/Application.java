8f7d0f43a1f7291624a93dd8939bf2663d8f68ca
nam <giangnam.trunghoccoso@gmail.com>
2024-03-26 00:26:42 +0700

    Init project

---
+++ src/main/java/com/gui/swing/Application.java
@@ -0,0 +1,26 @@
+package com.gui.swing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		// Demo Simple Application
+		JFrame f=new JFrame();//creating instance of JFrame
+
+		JButton b=new JButton("click");//creating instance of JButton
+		b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+
+		f.add(b);//adding button in JFrame
+
+		f.setSize(400,500);//400 width and 500 height
+		f.setLayout(null);//using no layout managers
+		f.setVisible(true);//making the frame visible
+		SpringApplication.run(Application.class, args);
+	}
+
+}
